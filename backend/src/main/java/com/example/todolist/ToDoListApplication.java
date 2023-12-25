2803c8c4368ee7cc7e19ddc2a523de0ba50a80b3
Csaba Birkás <bcsaba04082@gmail.com>
2023-12-25 15:34:54 +0100

    CORS disabled
    CORS disabled successfully.

--- backend/src/main/java/com/example/todolist/ToDoListApplication.java
+++ backend/src/main/java/com/example/todolist/ToDoListApplication.java
@@ -2,9 +2,12 @@ package com.example.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ToDoListApplication {
@@ -13,4 +16,13 @@ public class ToDoListApplication {
         SpringApplication.run(ToDoListApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*");
+            }
+        };
+    }
 }
