2bd61981fedf662b28f1ee8121e4b13addd4fcbf
Csaba Birkás <bcsaba04082@gmail.com>
2023-12-25 14:51:04 +0100

    adding frontend controller file
    added frontend controller script file. The requests are still blocked because of backend CORS

---
+++ frontend/resources/scripts/BackendController.js
@@ -0,0 +1,15 @@
+const apiUrl = "http://localhost:8080";
+
+fetch(apiUrl + "/task/tasks/1")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
