3656a2c39d85645b2500ae9450088a71a95cb544
Csaba Birkás <bcsaba04082@gmail.com>
2024-03-17 16:17:03 +0100

    saveSettings function added
    Added saveSetting function in the frontend, Data passed correctly to it.

--- frontend/resources/scripts/SettingsController.js
+++ frontend/resources/scripts/SettingsController.js
@@ -1,13 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const select = document.getElementById("imageSelect");
-  const images = document.querySelectorAll("#imageContainer img");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const select = document.getElementById("imageSelect");
+//   const images = document.querySelectorAll("#imageContainer img");
 
-  images.forEach((img) => {
-    img.addEventListener("click", function () {
-      const value = this.getAttribute("data-value");
-      select.value = value;
-    });
-  });
-});
+//   images.forEach((img) => {
+//     img.addEventListener("click", function () {
+//       const value = this.getAttribute("data-value");
+//       select.value = value;
+//     });
+//   });
+// });
 
-function saveSettings() {}
+function saveSettings() {
+  let pictureNumber = document.querySelector(
+    'input[name="background-select"]:checked'
+  ).value;
+  console.log(pictureNumber);
+}
