3806fa5e491cf99c0a71ffe3a47d420babcb8e2d
Csaba Birkás <bcsaba04082@gmail.com>
2024-03-30 11:36:13 +0100

    backend background settings
    Backend can now have functionality to change background when its called

--- backend/src/main/java/com/example/todolist/controller/SettingsController.java
+++ backend/src/main/java/com/example/todolist/controller/SettingsController.java
@@ -1,4 +1,30 @@
 package com.example.todolist.controller;
 
+
+import com.example.todolist.model.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+@RestController
+@RequestMapping(path = "/settings")
 public class SettingsController {
+
+    private String filePath = "../frontend/resources/settings.json";
+
+
+    @RequestMapping(value = "/save/{backgroundId}", method = RequestMethod.POST)
+    public ResponseEntity<?> createTask(@PathVariable(name = "backgroundId") Integer backgroundId){
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            // Convert Settings object to JSON and write to file
+            fileWriter.write("{ \"background_number\": " + backgroundId + "}");
+            return ResponseEntity.ok().build();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
